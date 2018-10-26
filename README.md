ced0632585654f8f9f69e742bb4aab643c3243ce
Matt <moldycrow55@gmail.com>
2018-10-26 13:59:10 -0500

    Finished

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 Week 4 homework assignment:
 Crystals Collector Game
 
-This is a guessing game using numbers where the player will guess with numbers associated with photos of crystals.
-There is four crystals displayed as buttons on the page.
-The player will be shown a random number at the start of the game.
-When the player clicks on a crystal it will add a specific amount of points to the player's total score.
-Points for each crystal is not shown to the player, but their total score is displayed then updated after each crystal is clicked.
-Games won and lost will be accumulated and shown to the player.
-If the player matches their total score to the random number they get one win. If the player's total score is above or below the random number, the player receives one loss.
-When restarted, player gets new random number and player's total score reset to zero. 
+   * There will be four crystals displayed as buttons on the page.
+
+   * The player will be shown a random number at the start of the game.
+
+   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+   * The player wins if their total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever the player wins or loses.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,17 +11,14 @@ var currentLossesCount = 0;
 var currentUserScore = 0;
 var gemImgArr = ["assets/images/blue-crystal.jpg", "assets/images/red-crystal.jpg", "assets/images/green-crystal.jpg", "assets/images/yellow-crystal.jpg"]
 
-//define script that must be run after page is loaded
 $(document).ready(function() {
 
-    //initialize target, gem values and reset user score to 0
     function initializeRandomValues() {
         randomTarget = Math.floor((Math.random() * 120) + 19);
         currentUserScore = 0;
 
         for (var i = 0; i < crystalsArr.length; i++) {
             var tempRandom = Math.floor((Math.random() * 12) + 1);
-            //Ensure the random numbers for the 4 gems are unique
             if (crystalValueArray.indexOf(tempRandom) == -1) {
                 crystalValueArray[i] = tempRandom;
             }
@@ -30,21 +27,15 @@ $(document).ready(function() {
             }
         }
     }    
-
-    //Get initial random values
     initializeRandomValues();
-
-    //initialize Target Display
     var TargetDiv = $(".empty-div-target");
     newTargetDiv = $("<div>");
     newTargetDiv.text(randomTarget);
     TargetDiv.append(newTargetDiv);
     TargetDiv.attr("class", "target");
     
-    //initialize Win-Loss Display
+    //Win-Loss Display
     var WinLossDiv = $(".empty-win-lose");
-    
-    //Win Display with second display to allow easier update of the number of wins
     var newWinP = $("<p>");
     var newWinCountSpan = $("<span>");
     newWinP.text("Wins: ");
@@ -55,7 +46,6 @@ $(document).ready(function() {
     WinLossDiv.append(newWinP);
     WinLossDiv.attr("class", "win-lose");
 
-    //Win Display with second display to allow easier update of the number of wins
     var newLossP = $("<p>");
     var newLossCountSpan = $("<span>");
     newLossP.text("Losses: ");
@@ -73,17 +63,13 @@ $(document).ready(function() {
     userScoreDiv.append(newScoreDiv);
     userScoreDiv.attr("class", "score");
 
-    //initialize gemstones display
     for (var k = 0; k < crystalsArr.length; k++) {
         var gemImage = $("<img>");
         gemImage.addClass("gemstones-img");
         gemImage.attr("gemColor", crystalsArr[k]);
         gemImage.attr("src", gemImgArr[k]);
-        // console.log("gemImage.attr('gemColor') = " + gemImage.attr("gemColor"));
         $(".empty-gemstones").append(gemImage);
     }
-
-    //initialize restart button and add it to gemstone display - then hide it
     restartBtn = $("<button>");
     restartBtn.addClass("gemstones-img");
     restartBtn.text("New Game");
@@ -92,7 +78,6 @@ $(document).ready(function() {
 
 
     function updateDisplay() {
-        //update display values
         newTargetDiv.text(randomTarget);
         newWinCountSpan.text(currentWinCount);
         newLossCountSpan.text(currentLossesCount);
@@ -101,14 +86,12 @@ $(document).ready(function() {
 
     //Process gem selection event
     $(".gemstones-img").on("click", function() {
-        //When the user wins or losses, ignore gem clicks until the new game button is selected
         if (!newGame) {
             var GemColorSelected = $(this).attr("gemColor");
         
-            //add specific gem value to user score
             currentUserScore += crystalValueArray[crystalsArr.indexOf(GemColorSelected)];
 
-            //update user score display
+            //update user score 
             newScoreDiv.text(currentUserScore);
 
             if (currentUserScore >= randomTarget) {
@@ -122,7 +105,6 @@ $(document).ready(function() {
                     "Green = " + crystalValueArray[2] + "\n" +
                     "Yellow = " + crystalValueArray[3] + "\n");
                     newGame = true;
-                    //show the restart button
                     restartBtn.show();
                 }
                 else {
@@ -135,26 +117,17 @@ $(document).ready(function() {
                     "Green = " + crystalValueArray[2] + "\n" +
                     "Yellow = " + crystalValueArray[3] + "\n");               
                     newGame = true;
-                    //show the restart button
                     restartBtn.show();
                 }
-                
                 updateDisplay();
-
             }
         }
-
-    }) //End of Gem Click event processing
-
-    // Process Restart Button selection
+    })
     restartBtn.on("click", function() {
         newGame = false;
         initializeRandomValues();
         updateDisplay();
-        //Hide restart button
         restartBtn.hide();
-        
-    }) //End of restart button click event processing
-
-}); //End of document ready section
+    }) 
+}); 
     
